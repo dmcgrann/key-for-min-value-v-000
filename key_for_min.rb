7907fc6f356a40c078ae7ad644a03eafa354638f
David McGrann <dpmcgrann@gmail.com>
2018-03-14 00:27:09 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,11 +3,11 @@
 
 def key_for_min_value(name_hash)
   smallest_name = nil
-  smallest_age = Float::INFINITY
-  name_hash.each do |name, age|
-    if age < smallest_age
-      smallest_age = age
-      smallest_name = name
+  smallest_age = nil
+  name_hash.each do |k, v|
+    if smallest_age == nil || v < smallest_age
+      smallest_age = v
+      smallest_name = k
     end
   end
   smallest_name
